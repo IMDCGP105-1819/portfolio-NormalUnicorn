7d1819167f57e758d8da7484cd6e38590c5bbb6d
Jordan <JordanChell@vivaldi.net>
2018-10-04 11:06:34 +0100

    This creates a function that will print out messages depending on the user inputs, also adds a dev function to prevent having to put inputs in every time the program is run

--- ex6.py
+++ ex6.py
@@ -1,3 +1,23 @@
+# This code should start printing out messages
+def messages(name, age, height, weight, eyes, hair):
+    if age < 10:
+        print("Hi there youn'un")
+    elif age >= 10 and age < 20:
+        print("Hi there!")
+    elif age >= 20:
+        print("Hi there grandpa!")
+
+# Saves me retying inputs all the time
+def dev_mode():
+    name = "Jordan"
+    age = 15
+    height = 150
+    weight = 90
+    eyes = "Green"
+    hair = "Brown"
+    messages(name, age, height, weight, eyes, hair)
+
+# This asks the user all the inputs
 def user_input():
     name = str(input("Please enter your name: "))
     age = int(input("Please enter how old you are (in years): "))
@@ -5,8 +25,12 @@ def user_input():
     weight = int(input("Please enter your weight (to the nearest kg): "))
     eyes = str(input("Please enter your eye colour: "))
     hair = str(input("Please enter your hair colour: "))
-    print(str(name), str(age), str(height), str(weight), str(eyes), str(hair))
+    messages(name, age, height, weight, eyes, hair)
 
+# If satement/variable to save me time from entering in the inputs
+dev = 1
 
-
-user_input()
+if dev == 0:
+    user_input()
+else:
+    dev_mode()
