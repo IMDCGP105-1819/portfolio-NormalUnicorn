b77d74188d8f35936033e156dd3ce4333f4a8653
Jordan <JordanChell@vivaldi.net>
2018-10-04 10:22:28 +0100

    this prints out all of the messages of beer bottles from 99 to 0

---
+++ ex5.py
@@ -0,0 +1,3 @@
+for x in range(1,100):
+    print(str(100-x) + " bottles of beer on the wall, " + str(100-x) + " bottles of beer.")
+    print("Take one down, pass it around, " + str((100-x)-1) + " bottles of beer on the wall...")
