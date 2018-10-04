4204e2f4487d1962f8a4c9183ad974ebf77a346a
Jordan <JordanChell@vivaldi.net>
2018-10-04 11:35:40 +0100

    creates input variables

---
+++ ex7.py
@@ -0,0 +1,7 @@
+def inputs():
+    total_cost = int(input("Please enter how much your ideal house cost:"))
+    annual_salary = int(input("Please enter your annual salary:"))
+    monthly_savings = int(input("Please enter how much you want to save per month:"))
+    print(total_cost, annual_salary, monthly_savings)
+
+inputs()
