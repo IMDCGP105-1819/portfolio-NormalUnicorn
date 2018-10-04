c6c588caaac23b376f88104b40563762dc0715d2
Jordan <JordanChell@vivaldi.net>
2018-10-04 11:50:35 +0100

    This now includes the maths for working out how many months you have to save

--- ex7.py
+++ ex7.py
@@ -1,7 +1,17 @@
+def maths(total_cost, annual_salary, monthly_savings):
+    deposit = total_cost*0.2
+    current_savings = 0
+    counter = 0
+    while current_savings < deposit and counter != 250:
+        current_savings += current_savings*(0.04/12) + (annual_salary*monthly_savings)
+        counter +=1
+    print(counter)
+
+
 def inputs():
-    total_cost = int(input("Please enter how much your ideal house cost:"))
-    annual_salary = int(input("Please enter your annual salary:"))
-    monthly_savings = int(input("Please enter how much you want to save per month:"))
-    print(total_cost, annual_salary, monthly_savings)
+    total_cost = float(input("Please enter how much your ideal house cost:"))
+    annual_salary = float(input("Please enter your annual salary:"))
+    monthly_savings = float(input("Please enter how much you want to save per month:"))
+    maths(total_cost, annual_salary, monthly_savings)
 
 inputs()
