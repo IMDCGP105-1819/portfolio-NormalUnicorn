8734629f7c9554f18b248ef2c343f12ea8554a9e
Jordan <JordanChell@vivaldi.net>
2018-10-11 10:12:23 +0100

    first attempt at logic for savings

--- Weekly_Exercise/Y1S1/ex8.py
+++ Weekly_Exercise/Y1S1/ex8.py
@@ -3,8 +3,11 @@ def maths(total_cost, annual_salary, monthly_savings, semi_annual_raise):
     current_savings = 0
     counter = 0
     while current_savings < deposit and counter != 250:
-        current_savings += current_savings*(0.04/12) + (annual_salary*monthly_savings)
-        counter +=1
+        for x in range(0,5): #this loop acts as a way to calculate the impact of the raise
+            current_savings += current_savings*(0.04/12) + (annual_salary*monthly_savings)
+            counter +=1
+        counter += 5
+        monthly_savings = monthly_savings*semi_annual_raise
     print(counter)
 
 
@@ -12,8 +15,13 @@ def inputs():
     total_cost = float(input("Please enter how much your ideal house cost:"))
     annual_salary = float(input("Please enter your annual salary:"))
     monthly_savings = float(input("Please enter how much you want to save per month:"))
-    semi_annual_raise = float("Please enter how much of a raise you get every 6 months")
+    semi_annual_raise = float(input("Please enter how much of a raise you get every 6 months"))
 
     maths(total_cost, annual_salary, monthly_savings, semi_annual_raise)
 
-inputs()
+dev = 1
+
+if dev == 0:
+    inputs()
+else:
+    maths(1000000, 10000, 1500, 0.07)
