669fa92350dda02a7a9f041ec1261acc80a7c04f
Jordan <JordanChell@vivaldi.net>
2018-10-11 11:41:12 +0100

    working code after lots of trial and error

--- Weekly_Exercise/Y1S1/ex8.py
+++ Weekly_Exercise/Y1S1/ex8.py
@@ -1,27 +1,20 @@
-def maths(total_cost, annual_salary, monthly_savings, semi_annual_raise):
-    deposit = total_cost*0.2
-    current_savings = 0
-    counter = 0
-    while current_savings < deposit and counter != 250:
-        for x in range(0,5): #this loop acts as a way to calculate the impact of the raise
-            current_savings += current_savings*(0.04/12) + (annual_salary*monthly_savings)
-            counter +=1
-        counter += 5
-        monthly_savings = monthly_savings*semi_annual_raise
-    print(counter)
+total_cost = float(input("Please enter how much your ideal house should cost:"))
+annual_salary = float(input("Please enter your annual salary:"))
+monthly_savings_value = float(input("Please enter the %  of your annual salary you intend to save each month:"))
+monthly_savings = (annual_salary/12)*monthly_savings_value
+semi_annual_raise = float(input("Please enter how much of a raise you receieve every 6 months"))
+#All of these variables are used input based
 
+deposit = total_cost*0.2
+current_savings = 0.0
+months = 1
 
-def inputs():
-    total_cost = float(input("Please enter how much your ideal house cost:"))
-    annual_salary = float(input("Please enter your annual salary:"))
-    monthly_savings = float(input("Please enter how much you want to save per month:"))
-    semi_annual_raise = float(input("Please enter how much of a raise you get every 6 months"))
+while current_savings < deposit and months != 250:
+    if months % 6 == 0:
+        annual_salary = annual_salary + (annual_salary*semi_annual_raise)
+        monthly_savings = (annual_salary/12)*monthly_savings_value
+    current_savings += current_savings*(0.04/12) + monthly_savings
+    print(current_savings)
+    months += 1
 
-    maths(total_cost, annual_salary, monthly_savings, semi_annual_raise)
-
-dev = 1
-
-if dev == 0:
-    inputs()
-else:
-    maths(1000000, 10000, 1500, 0.07)
+print(months)
