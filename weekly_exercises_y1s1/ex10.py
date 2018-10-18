f41ea7658878c7b669434534ce8626950c8c669b
Jordan <JordanChell@vivaldi.net>
2018-10-18 09:34:34 +0100

    adding inputs for values

---
+++ weekly_exercises_y1s1/ex10.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+def inputs()
+    low_val = int(input("Please enter a lower value to start searching from"))
+    high_val = int(input("Please enter a high value to start searching from"))
+
+inputs
