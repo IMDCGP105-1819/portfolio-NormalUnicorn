b3002300d2b0c0312b8597f4fec759246097c336
Jordan <JordanChell@vivaldi.net>
2018-10-18 10:01:02 +0100

    added a guess function

--- weekly_exercises_y1s1/ex11.py
+++ weekly_exercises_y1s1/ex11.py
@@ -1,1 +1,24 @@
-from random import randomint 
+import random
+
+def guesses(random_int):
+    past_guesses = []
+    guess = 0
+    while guess != random_int:
+        guess = int(input("Please enter a number to guess"))
+        if guess != random_int:
+            past_guesses.extend(str(guess))
+
+        if guess < random_int:
+            print("That guess is too low!")
+
+        if guess > random_int:
+            print("That guess is too high")
+            
+
+def values():
+    low_val = int(input("Please enter a lower value"))
+    high_val = int(input("Please enter a higher value"))
+    random_int = random.randint(low_val, high_val)
+    guesses(random_int)
+
+values()
