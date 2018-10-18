927248809d9aa3be0282082660fcd7c9a3c09d44
Jordan <JordanChell@vivaldi.net>
2018-10-18 09:40:34 +0100

    added fizzbuzz check

--- weekly_exercises_y1s1/ex10.py
+++ weekly_exercises_y1s1/ex10.py
@@ -1,10 +1,20 @@
+def fizzbuzz(low, high):
+    i = low
+    for i in range(low, high+1):
+        # high + 1 so that it includes the high value
+        output = ""
+        if i % 3 == 0:
+            output += "Fizz"
 
+        if i % 5 == 0:
+            output += "Buzz"
 
+        print(output, i)
+        # print out i so that you can check if the value is fizz buzz or not
 
-
-
-def inputs()
+def inputs():
     low_val = int(input("Please enter a lower value to start searching from"))
-    high_val = int(input("Please enter a high value to start searching from"))
+    high_val = int(input("Please enter a high value to search up to"))
+    fizzbuzz(low_val, high_val)
 
-inputs
+inputs()
